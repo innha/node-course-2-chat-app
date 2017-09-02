db768c924555a0d5b1b3f2c800912af17ab33437
innha <iharelimana@gmail.com>
2017-09-02 10:35:14 +0200

    Add form for messages and show incming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,12 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: 'Hey. Joining u.'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'User',
+  //   text: 'Hey. Joining u.'
+  // }, function (data) {
+  //   console.log('Got it', data);
+  // });
 });
 
 socket.on('disconnect', function () {
@@ -15,4 +17,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
