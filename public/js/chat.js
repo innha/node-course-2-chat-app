41a361cd43e2869c7c00c6e92aca60f4ffd5bbf5
innha <iharelimana@gmail.com>
2017-09-03 11:58:03 +0200

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,7 @@ function scrollToBottom () {
 };
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  // console.log('Connected to server');
 
   // socket.emit('createMessage', {
   //   from: 'User',
@@ -24,11 +24,33 @@ socket.on('connect', function () {
   // }, function (data) {
   //   console.log('Got it', data);
   // });
+
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err); //can use bootstrap/foundation
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
-})
+});
+
+socket.on('updateUserList', function (users) {
+  // console.log('Users list', users);
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
 
 socket.on('newMessage', function (message) {
   // console.log('newMessage', message);
